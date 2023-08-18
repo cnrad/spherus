ab7d94c20fed1a4774b9358c584cb879960c08f7
cnrad <conradwcrawford@gmail.com>
2023-08-18 15:28:39 -0400

    feat: responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const variants = {
   animate: {
     opacity: 1,
     y: 0,
-    transition: { duration: 1, ease: "easeOut", staggerChildren: 0.1 },
+    transition: { duration: 0.75, ease: "easeOut", staggerChildren: 0.1 },
   },
 };
 
@@ -30,15 +30,15 @@ export default function Home() {
         initial={"initial"}
         animate={"animate"}
         variants={variants}
-        className={`flex min-h-[35rem] flex-col items-center justify-center text-white mb-16 w-min gap-2`}
+        className={`flex min-h-[35rem] flex-col items-center justify-center text-white md:mb-16 w-min gap-2`}
       >
-        <h1 className="font-extralight text-[13rem] h-min leading-none">
+        <h1 className="font-extralight text-[4rem] md:text-[8rem] lg:text-[13rem] h-min leading-none">
           SPHERUS.
         </h1>
-        <h2 className="text-zinc-200 font-light text-3xl tracking-widest">
+        <h2 className="text-zinc-200 font-light text-center text-xl md:text-3xl tracking-widest">
           a collection of attempts to convey ineffable emotions.
         </h2>
-        <h3 className="text-zinc-400 font-light text-xl flex w-full justify-between px-4 tracking-wider">
+        <h3 className="text-zinc-400 font-light text-lg text-center lg:text-start md:text-xl flex flex-col lg:flex-row w-full justify-between px-4 tracking-wider">
           <div>
             <span className="text-zinc-200 font-normal text-2xl mr-2">
               sphe·ra
@@ -58,13 +58,13 @@ export default function Home() {
         initial={"initial"}
         animate={"animate"}
         variants={variants}
-        className="h-auto grid grid-cols-2 gap-28 mb-32"
+        className="h-auto flex flex-col 2xl:grid 2xl:grid-cols-2 gap-28 mb-32"
       >
         {spherusData.map((spherus) => (
           <motion.div
             key={spherus.word}
             variants={variants}
-            className="w-min flex flex-row items-start justify-evenly gap-12 h-[20rem]"
+            className="w-min h-auto flex flex-col items-center md:flex-row md:items-start justify-evenly md:gap-12 md:h-[20rem]"
           >
             <img
               alt={spherus.word}
@@ -72,7 +72,7 @@ export default function Home() {
               className="relative min-w-[20rem] rounded-md cursor-pointer transition-all duration-150 hover:brightness-75"
               onClick={() => setCurrentModal(spherus.word)}
             />
-            <div className="flex flex-col min-w-[25rem] h-full">
+            <div className="flex mt-8 md:mt-0 flex-col min-w-[80vw] md:min-w-[25rem] h-full">
               <h1 className="text-[3rem] leading-none font-semibold capitalize mb-2">
                 {spherus.word}
               </h1>
@@ -84,7 +84,7 @@ export default function Home() {
               <a
                 href={`https://cnrad.darkroom.com/`}
                 rel="noreferrer noopener"
-                className="mt-auto text-center px-3 py-2 bg-zinc-900 text-zinc-400 rounded-md hover:bg-zinc-800 border border-zinc-900 hover:border-zinc-700 hover:text-white transition-colors duration-150"
+                className="mt-4 md:mt-auto text-center px-3 py-2 bg-zinc-900 text-zinc-400 rounded-md hover:bg-zinc-800 border border-zinc-900 hover:border-zinc-700 hover:text-white transition-colors duration-150"
               >
                 Purchase Print
               </a>
@@ -93,7 +93,7 @@ export default function Home() {
         ))}
       </motion.div>
 
-      <footer className="py-4 flex items-center justify-center text-sm text-zinc-500 w-full">
+      <footer className="py-4 text-sm text-zinc-500 w-full text-center">
         All works © Conrad Crawford 2023. Please do not reproduce without the
         expressed written consent of Conrad Crawford
       </footer>
@@ -114,7 +114,7 @@ export default function Home() {
             {currentModal && (
               <img
                 alt={currentModal}
-                className="relative h-[80vh] w-auto rounded-md"
+                className="relative w-[80vw] h-auto md:max-w-[50rem] md:w-auto md:min-h-[50rem] rounded-md"
                 src={`/spherus/${currentModal}.jpg`}
               />
             )}

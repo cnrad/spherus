17a115c584a3c9ea647c9f8cab991ecf4d9c16e5
cnrad <conradwcrawford@gmail.com>
2023-08-18 14:43:33 -0400

    main content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,124 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { SpherusModal } from "@/src/SpherusModal";
+import { spherusData } from "@/public/spherus/spherusData";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"], style: "normal" });
+const variants = {
+  initial: { opacity: 0, y: 40 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: "easeOut", staggerChildren: 0.1 },
+  },
+};
 
 export default function Home() {
+  const [currentModal, setCurrentModal] = useState<string | null>(null);
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") setCurrentModal(null);
+    });
+
+    return window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") setCurrentModal(null);
+    });
+  });
+
   return (
-    <>
-      <div
-        className="fade-blur fixed w-full top-0 h-[20rem] z-10 pointer-events-none opacity-95"
-        style={{
-          maskImage: "linear-gradient(to bottom,#000 25%, transparent)",
-          backdropFilter: "blur(5px)",
-        }}
-      />
-
-      <main
-        className={`flex h-auto flex-col items-center justify-start text-white overflow-y-scroll snap-y snap-mandatory`}
+    <main className="max-h-screen min-h-screen flex flex-col items-center overflow-y-scroll">
+      <motion.div
+        initial={"initial"}
+        animate={"animate"}
+        variants={variants}
+        className={`flex min-h-[35rem] flex-col items-center justify-center text-white mb-16 w-min gap-2`}
       >
-        <div className="h-screen flex flex-col items-center justify-center w-min gap-2">
-          <h1 className="font-extralight text-[13rem] h-min leading-none">
-            SPHERUS.
-          </h1>
-          <h2 className="text-zinc-200 font-light text-3xl tracking-widest">
-            a collection of attempts to convey ineffable emotions.
-          </h2>
-          <h3 className="text-zinc-400 font-light text-xl flex w-full justify-between px-4 tracking-wider">
-            <div>
-              <span className="text-zinc-200 font-normal text-2xl mr-2">
-                sphe·ra
-              </span>
-              globe, sphere, orb, ball.
-            </div>
-            <div>
-              <span className="text-zinc-200 font-normal text-2xl mr-2">
-                sēn·sus
-              </span>
-              perception, feeling, sensation, sense.
-            </div>
-          </h3>
-        </div>
+        <h1 className="font-extralight text-[13rem] h-min leading-none">
+          SPHERUS.
+        </h1>
+        <h2 className="text-zinc-200 font-light text-3xl tracking-widest">
+          a collection of attempts to convey ineffable emotions.
+        </h2>
+        <h3 className="text-zinc-400 font-light text-xl flex w-full justify-between px-4 tracking-wider">
+          <div>
+            <span className="text-zinc-200 font-normal text-2xl mr-2">
+              sphe·ra
+            </span>
+            globe, sphere, orb, ball.
+          </div>
+          <div>
+            <span className="text-zinc-200 font-normal text-2xl mr-2">
+              sēn·sus
+            </span>
+            perception, feeling, sensation, sense.
+          </div>
+        </h3>
+      </motion.div>
 
-        <div className="h-[100vh] w-[10rem] flex items-center justify-center">
-          test
-        </div>
-      </main>
-    </>
+      <motion.div
+        initial={"initial"}
+        animate={"animate"}
+        variants={variants}
+        className="h-auto grid grid-cols-2 gap-28 mb-32"
+      >
+        {spherusData.map((spherus) => (
+          <motion.div
+            variants={variants}
+            className="w-min flex flex-row items-start justify-evenly gap-12 h-[20rem]"
+          >
+            <img
+              src={`/spherus/${spherus.word}.jpg`}
+              className="relative min-w-[20rem] rounded-md cursor-pointer transition-all duration-150 hover:brightness-75"
+              onClick={() => setCurrentModal(spherus.word)}
+            />
+            <div className="flex flex-col min-w-[25rem] h-full">
+              <h1 className="text-[3rem] leading-none font-semibold capitalize mb-2">
+                {spherus.word}
+              </h1>
+              <h3 className="text-2xl text-zinc-300 mb-4 italic font-light">
+                {spherus.meta}
+              </h3>
+              <p className="text-xl text-zinc-300 font-light">{spherus.desc}</p>
+
+              <a
+                href={`https://cnrad.darkroom.com/`}
+                rel="noreferrer noopener"
+                className="mt-auto text-center px-3 py-2 bg-zinc-900 text-zinc-400 rounded-md hover:bg-zinc-800 border border-zinc-900 hover:border-zinc-700 hover:text-white transition-colors duration-150"
+              >
+                Purchase Print
+              </a>
+            </div>
+          </motion.div>
+        ))}
+      </motion.div>
+
+      <footer className="py-4 flex items-center justify-center text-sm text-zinc-500 w-full">
+        All works © Conrad Crawford 2023. Please do not reproduce without the
+        expressed written consent of Conrad Crawford
+      </footer>
+
+      <AnimatePresence>
+        {currentModal && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.25, ease: "easeInOut" }}
+            className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center"
+          >
+            <div
+              className="absolute w-screen min-h-screen bg-black opacity-80"
+              onClick={() => setCurrentModal(null)}
+            />
+            {currentModal && (
+              <img
+                className="relative h-[80vh] w-auto rounded-md"
+                src={`/spherus/${currentModal}.jpg`}
+              />
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </main>
   );
 }
---
+++ public/spherus/spherusData.ts
@@ -0,0 +1,52 @@
+export const spherusData = [
+  {
+    word: "amaetria",
+    meta: "a·mae·tri·a (noun)",
+    desc: "the realization that you'll never be able to relive a certain memory the same way, ever again.",
+  },
+  {
+    word: "verenquillum",
+    meta: "ver·en·quill·um (noun)",
+    desc: "the moment one finds a fleeting state of mental serenity in a loud, chaotic environment.",
+  },
+  {
+    word: "pavimudo",
+    meta: "pa·vi·mu·do (noun)",
+    desc: "the overwhelming mix of awe and insignificance when faced with the scale of our world.",
+  },
+  {
+    word: "amacoridum",
+    meta: "a·ma·cor·i·dum (noun)",
+    desc: "when being with your heart's companion causes all darkness in the world to vanish, even if just for a moment.",
+  },
+  {
+    word: "laemidum",
+    meta: "lae·mi·dum (noun)",
+    desc: "the vivid, vibrant colors found in one's recollection of childhood experiences.",
+  },
+  {
+    word: "praemorit",
+    meta: "prae·mor·it (adj.)",
+    desc: "to be resentful of a broken past, forever dwelling in the old while the new is ignored.",
+  },
+  {
+    word: "consitrae",
+    meta: "con·sit·rae (noun)",
+    desc: "to be uncertain of the future, stuck exploring every possible outcome, so much so that you become blind to what's right in front of you.",
+  },
+  {
+    word: "galaevita",
+    meta: "ga·lae·vi·ta (noun)",
+    desc: "the inexpressible joy that follows realizing that you are exactly where you've strived to be for so long.",
+  },
+  {
+    word: "vibreulo",
+    meta: "vi·bre·u·lo (noun)",
+    desc: "when the motions of everyday life feel unreal, as if reality itself is merely an illusion, yet all logical thinking proves the opposite.",
+  },
+  {
+    word: "robicelium",
+    meta: "ro·bi·ce·li·um (noun)",
+    desc: "a distant hopefulness for the future, veiled in waves of doubt.",
+  },
+];
